e65a5308eff0d761d90bf7f3db179cfa882d3f39
Moein Siami <moeins1216@gmail.com>
2025-01-23 11:28:21 -0600

    update models

--- Blog_API/models.py
+++ Blog_API/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.CharField(max_length=100)
+    tags = ArrayField(models.CharField(max_length=100, blank=True))
+    createdAt = models.DateTimeField(auto_now=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
