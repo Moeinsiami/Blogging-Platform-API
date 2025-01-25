18886b41e87258587131567971d14f4ac5f0e98f
Moein Siami <moeins1216@gmail.com>
2025-01-25 01:07:16 -0600

    add CRUD views

--- Blog_API/views.py
+++ Blog_API/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class CreatePostView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UpdatePostView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class DeletePostView(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class GetPostView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
