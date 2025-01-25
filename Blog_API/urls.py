a7c9cad05a90fe5835d760bbdc037686867d55c7
Moein Siami <moeins1216@gmail.com>
2025-01-25 01:07:06 -0600

    add CRUD urls

--- Blog_API/urls.py
+++ Blog_API/urls.py
@@ -1,8 +1,12 @@
-from . import views
+from .views import *
 from django.urls import path
+from .views import CreatePostView, UpdatePostView
 
 app_name = 'Blog_API'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('create/', CreatePostView.as_view(), name='create-post'),
+    path('/update/<int:pk>/', UpdatePostView.as_view(), name='update-post'),
+    path('<int:pk>', DeletePostView.as_view(), name='delete-post'),
+    path('post/<int:pk>/', GetPostView.as_view(), name='get-post'),
 ]
